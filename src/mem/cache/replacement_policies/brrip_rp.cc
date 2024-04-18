5b4106f417feed649d697a4da8fb248bb57b1bda
alpylmz <>
2024-04-18 15:58:52 -0400

    cleaned brrip

--- src/mem/cache/replacement_policies/brrip_rp.cc
+++ src/mem/cache/replacement_policies/brrip_rp.cc
@@ -42,10 +42,8 @@ namespace replacement_policy
 {
 
 BRRIP::BRRIP(const Params &p)
-  : Base(p), numRRPVBits(p.num_bits), hitPriority(p.hit_priority),
-    btp(p.btp)
+  : Base(p)
 {
-    fatal_if(numRRPVBits <= 0, "There should be at least one bit per RRPV.\n");
 }
 
 void
@@ -63,15 +61,6 @@ BRRIP::touch(const std::shared_ptr<ReplacementData>& replacement_data) const
 {
     std::shared_ptr<BRRIPReplData> casted_replacement_data =
         std::static_pointer_cast<BRRIPReplData>(replacement_data);
-
-    // Update RRPV if not 0 yet
-    // Every hit in HP mode makes the entry the last to be evicted, while
-    // in FP mode a hit makes the entry less likely to be evicted
-    if (hitPriority) {
-        casted_replacement_data->rrpv.reset();
-    } else {
-        casted_replacement_data->rrpv--;
-    }
 }
 
 void
@@ -80,13 +69,6 @@ BRRIP::reset(const std::shared_ptr<ReplacementData>& replacement_data) const
     std::shared_ptr<BRRIPReplData> casted_replacement_data =
         std::static_pointer_cast<BRRIPReplData>(replacement_data);
 
-    // Reset RRPV
-    // Replacement data is inserted as "long re-reference" if lower than btp,
-    // "distant re-reference" otherwise
-    casted_replacement_data->rrpv.saturate();
-    if (random_mt.random<unsigned>(1, 100) <= btp) {
-        casted_replacement_data->rrpv--;
-    }
 
     // Mark entry as ready to be used
     casted_replacement_data->valid = true;
@@ -95,48 +77,7 @@ BRRIP::reset(const std::shared_ptr<ReplacementData>& replacement_data) const
 ReplaceableEntry*
 BRRIP::getVictim(const ReplacementCandidates& candidates) const
 {
-    // There must be at least one replacement candidate
-    assert(candidates.size() > 0);
-
-    // Use first candidate as dummy victim
-    ReplaceableEntry* victim = candidates[0];
-
-    // Store victim->rrpv in a variable to improve code readability
-    int victim_RRPV = std::static_pointer_cast<BRRIPReplData>(
-                        victim->replacementData)->rrpv;
-
-    // Visit all candidates to find victim
-    for (const auto& candidate : candidates) {
-        std::shared_ptr<BRRIPReplData> candidate_repl_data =
-            std::static_pointer_cast<BRRIPReplData>(
-                candidate->replacementData);
-
-        // Stop searching for victims if an invalid entry is found
-        if (!candidate_repl_data->valid) {
-            return candidate;
-        }
-
-        // Update victim entry if necessary
-        int candidate_RRPV = candidate_repl_data->rrpv;
-        if (candidate_RRPV > victim_RRPV) {
-            victim = candidate;
-            victim_RRPV = candidate_RRPV;
-        }
-    }
-
-    // Get difference of victim's RRPV to the highest possible RRPV in
-    // order to update the RRPV of all the other entries accordingly
-    int diff = std::static_pointer_cast<BRRIPReplData>(
-        victim->replacementData)->rrpv.saturate();
-
-    // No need to update RRPV if there is no difference
-    if (diff > 0){
-        // Update RRPV of all candidates
-        for (const auto& candidate : candidates) {
-            std::static_pointer_cast<BRRIPReplData>(
-                candidate->replacementData)->rrpv += diff;
-        }
-    }
+    auto victim = candidates[0];
 
     return victim;
 }
@@ -144,7 +85,7 @@ BRRIP::getVictim(const ReplacementCandidates& candidates) const
 std::shared_ptr<ReplacementData>
 BRRIP::instantiateEntry()
 {
-    return std::shared_ptr<ReplacementData>(new BRRIPReplData(numRRPVBits));
+    return std::shared_ptr<ReplacementData>(new BRRIPReplData());
 }
 
 } // namespace replacement_policy
--- src/mem/cache/replacement_policies/brrip_rp.hh
+++ src/mem/cache/replacement_policies/brrip_rp.hh
@@ -69,47 +69,17 @@ class BRRIP : public Base
     /** BRRIP-specific implementation of replacement data. */
     struct BRRIPReplData : ReplacementData
     {
-        /**
-         * Re-Reference Interval Prediction Value.
-         * Some values have specific names (according to the paper):
-         * 0 -> near-immediate re-rereference interval
-         * max_RRPV-1 -> long re-rereference interval
-         * max_RRPV -> distant re-rereference interval
-         */
-        SatCounter8 rrpv;
-
         /** Whether the entry is valid. */
         bool valid;
 
         /**
          * Default constructor. Invalidate data.
          */
-        BRRIPReplData(const int num_bits)
-            : rrpv(num_bits), valid(false)
+        BRRIPReplData()
+            : valid(false)
         {
         }
     };
-
-    /**
-     * Number of RRPV bits. An entry that saturates its RRPV has the longest
-     * possible re-reference interval, that is, it is likely not to be used
-     * in the near future, and is among the best eviction candidates.
-     * A maximum RRPV of 1 implies in a NRU.
-     */
-    const unsigned numRRPVBits;
-
-    /**
-     * The hit priority (HP) policy replaces entries that do not receive cache
-     * hits over any cache entry that receives a hit, while the frequency
-     * priority (FP) policy replaces infrequently re-referenced entries.
-     */
-    const bool hitPriority;
-
-    /**
-     * Bimodal throtle parameter. Value in the range [0,100] used to decide
-     * if a new entry is inserted with long or distant re-reference.
-     */
-    const unsigned btp;
 
   public:
     typedef BRRIPRPParams Params;
