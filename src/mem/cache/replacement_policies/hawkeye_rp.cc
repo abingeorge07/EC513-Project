0cc3d32ea1431a7c190e45f68bbcf869ba7d82ce
alpylmz <>
2024-04-24 14:21:29 -0400

    seg fault debug

--- src/mem/cache/replacement_policies/hawkeye_rp.cc
+++ src/mem/cache/replacement_policies/hawkeye_rp.cc
@@ -29,6 +29,7 @@ Hawkeye::Hawkeye(const Params &p)
             temp_data.RRIP = 7;
             temp_data.index = 0;
             temp_data.tag = 0;
+            temp_data.first_touch = true;
             temp.push_back(temp_data);
         }
         RRIP_vector.push_back(temp);
@@ -57,49 +58,78 @@ void
 Hawkeye::touch(const std::shared_ptr<ReplacementData>& replacement_data, const PacketPtr pkt)
 {
     // get the PC from the packet
+    if(pkt->req->hasPC() == false){
+        //std::cout << "No PC found in the packet" << std::endl;
+        return;
+    }
+    //std::cout << "PC found in the packet" << std::endl;
     Addr pc = pkt->req->getPC();
     // get the first 13 bits of the PC
     int pc_index = pc & 0x1FFF;
     // get the address of the request
+    if(pkt->req->hasPaddr() == false){
+        //std::cout << "No address found in the packet" << std::endl;
+        return;
+    }
+    //std::cout << "Address found in the packet" << std::endl;
     Addr addr = pkt->req->getPaddr();
     // get the index bits
     int index_bits = (addr >> 3) & index_bit_mask;
     // get the tag bits
     uint64_t tag_bits = addr >> (3 + index_bit_count);
 
+    //std::cout << "before element get" << std::endl;
+
     // get the correct element
+    bool found = false;
     RRIPCacheData* element;
     for(int i = 0; i < RRIP_vector[index_bits].size(); i++){
         if(RRIP_vector[index_bits][i].tag == tag_bits){
             element = &RRIP_vector[index_bits][i];
+            found = true;
         }
     }
+    if(!found){
+        std::cout << "Element not found" << std::endl;
+        // we need to initialize it!
+        // and also we need to invalidate the entry from RRIP vector, too
+        // Like we are managing our own cache
+        return;
+    }
+
+    //std::cout << "after element get" << std::endl;
 
 
     bool prediction = true;
     // Assume prediction is done
     if(prediction == false){ // Averse
         // Update RRIP vector
+        //std::cout << "prediction is false" << std::endl;
         element->RRIP = 7;
     }
     else{
+        //std::cout << "prediction is true" << std::endl;
         if(element->first_touch){ // if hit
             // Update RRIP vector
+            std::cout << "Updating element->RRIP to 0" << std::endl;
             element->RRIP = 0;
         }
         else{ // if miss
             // Update RRIP vector
+            std::cout << "Updating RRIP vector" << std::endl;
             for(int i = 0; i < RRIP_vector[index_bits].size(); i++){
+                //std::cout << "index: " << i << std::endl;
                 if(RRIP_vector[index_bits][i].RRIP < 7){
                     RRIP_vector[index_bits][i].RRIP++;
                 }
             }
+            std::cout << "Updated RRIP vector" << std::endl;
             element->RRIP = 0;
         }
     }
 
     element->first_touch = false;
-
+    std::cout << "Finished touch" << std::endl;
 
 }
 
