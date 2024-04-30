0e9d13cac0b5d1e75f1a563b4fe1772e972550af
abingeorge07 <66330225+abingeorge07@users.noreply.github.com>
2024-04-29 21:05:16 -0400

    Add files via upload

--- bash-script/bash-deeps.sh
+++ bash-script/bash-deeps.sh
@@ -1,30 +1,30 @@
 #!/bin/bash -l
 
 #$ -l h_rt=72:00:00
-#$ -N perl
+#$ -N deeps
 #$ -pe omp 2
 
 source /projectnb/ec513/materials/HW6/spec-2017/sourceme
 
 cd /projectnb/ec513/students/abg309/gem5/HW6/spec-2017/gem5
 
-build/X86/gem5.opt --outdir="/projectnb/ec513/students/abg309/gem5/HW6/Milestone2/perlbench-1/" \
+build/X86/gem5.opt --outdir="/projectnb/ec513/students/abg309/gem5/HW6/Milestone2/deeps-1/" \
 configs/example/gem5_library/x86-spec-cpu2017-benchmarks-1.py \
 --image ../disk-image/spec-2017/spec-2017-image/spec-2017 \
 --partition 1 \
---benchmark 500.perlbench_r \
+--benchmark 531.deepsjeng_r \
 --size test
 
-build/X86/gem5.opt --outdir="/projectnb/ec513/students/abg309/gem5/HW6/Milestone2/perlbench-2/" \
+build/X86/gem5.opt --outdir="/projectnb/ec513/students/abg309/gem5/HW6/Milestone2/deeps-2/" \
 configs/example/gem5_library/x86-spec-cpu2017-benchmarks-2.py \
 --image ../disk-image/spec-2017/spec-2017-image/spec-2017 \
 --partition 1 \
---benchmark 500.perlbench_r \
+--benchmark 531.deepsjeng_r \
 --size test
 
-build/X86/gem5.opt --outdir="/projectnb/ec513/students/abg309/gem5/HW6/Milestone2/perlbench-4/" \
+build/X86/gem5.opt --outdir="/projectnb/ec513/students/abg309/gem5/HW6/Milestone2/deeps-4/" \
 configs/example/gem5_library/x86-spec-cpu2017-benchmarks-4.py \
 --image ../disk-image/spec-2017/spec-2017-image/spec-2017 \
 --partition 1 \
---benchmark 500.perlbench_r \
+--benchmark 531.deepsjeng_r \
 --size test
--- bash-script/bash-xz.sh
+++ bash-script/bash-xz.sh
@@ -1,30 +1,30 @@
 #!/bin/bash -l
 
 #$ -l h_rt=72:00:00
-#$ -N perl
+#$ -N xz
 #$ -pe omp 2
 
 source /projectnb/ec513/materials/HW6/spec-2017/sourceme
 
 cd /projectnb/ec513/students/abg309/gem5/HW6/spec-2017/gem5
 
-build/X86/gem5.opt --outdir="/projectnb/ec513/students/abg309/gem5/HW6/Milestone2/perlbench-1/" \
+build/X86/gem5.opt --outdir="/projectnb/ec513/students/abg309/gem5/HW6/Milestone2/xz-1/" \
 configs/example/gem5_library/x86-spec-cpu2017-benchmarks-1.py \
 --image ../disk-image/spec-2017/spec-2017-image/spec-2017 \
 --partition 1 \
---benchmark 500.perlbench_r \
+--benchmark 557.xz_r \
 --size test
 
-build/X86/gem5.opt --outdir="/projectnb/ec513/students/abg309/gem5/HW6/Milestone2/perlbench-2/" \
+build/X86/gem5.opt --outdir="/projectnb/ec513/students/abg309/gem5/HW6/Milestone2/xz-2/" \
 configs/example/gem5_library/x86-spec-cpu2017-benchmarks-2.py \
 --image ../disk-image/spec-2017/spec-2017-image/spec-2017 \
 --partition 1 \
---benchmark 500.perlbench_r \
+--benchmark 557.xz_r \
 --size test
 
-build/X86/gem5.opt --outdir="/projectnb/ec513/students/abg309/gem5/HW6/Milestone2/perlbench-4/" \
+build/X86/gem5.opt --outdir="/projectnb/ec513/students/abg309/gem5/HW6/Milestone2/xz-4/" \
 configs/example/gem5_library/x86-spec-cpu2017-benchmarks-4.py \
 --image ../disk-image/spec-2017/spec-2017-image/spec-2017 \
 --partition 1 \
---benchmark 500.perlbench_r \
+--benchmark 557.xz_r \
 --size test
